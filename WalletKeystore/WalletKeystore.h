63be5ad73bab293599797128e85f71d053413a92
Can Dang <candd1707@gmail.com>
2019-06-05 16:43:12 +0700

    Initial Commit

---
+++ WalletKeystore/WalletKeystore.h
@@ -0,0 +1,19 @@
+//
+//  WalletKeystore.h
+//  WalletKeystore
+//
+//  Created by Can Dang on 6/5/19.
+//  Copyright © 2019 Can Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WalletKeystore.
+FOUNDATION_EXPORT double WalletKeystoreVersionNumber;
+
+//! Project version string for WalletKeystore.
+FOUNDATION_EXPORT const unsigned char WalletKeystoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WalletKeystore/PublicHeader.h>
+
+
